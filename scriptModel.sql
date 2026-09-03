6770fb7507b4041c6708c118af8740ced00b1589
GiuliaTeixeira3626 <giulia.teixeira@sptech.school>
2026-09-03 15:07:23 -0300

    Script-Banco e Modelagem-Banco

--- scriptModel.sql
+++ scriptModel.sql
@@ -11,18 +11,13 @@ create table maquina (
     nucleosLogicos int,
     capacidadeTotal bigint,
     ramTotal bigint,
-    dtCadastro datetime
+    dtCadastro datetime,
+    fkEmpresa int,
+    constraint fkMaquinaEmpresa foreign key (fkEmpresa) references empresa(id)
 );
-
--- create table componentes (
--- 	id int primary key auto_increment,
---     nome varchar(45),
---     tipo varchar(45)
--- );
 
 create table registros (
 	idRegistro int auto_increment,
-  --  fkComponente int,
     fkMaquina int,
     cpuPorcentagemUso decimal (4,1),
     cpuFrequenciaAtual int,
@@ -33,17 +28,47 @@ create table registros (
     ramPercentualUso decimal(4,1),
     discoEspacoUsado int,
     discoEspacoLivre int,
+    downloadRede bigint,
+    uploadRede bigint,
     dtRegistro datetime,
 	constraint pkComposta primary key (idRegistro, fkMaquina),
---   constraint fkComponenteRegistro foreign key (fkComponente) references componentes(id),
     constraint fkMaquinaRegistro foreign key (fkMaquina) references maquina(id)
 );
 
-select * from maquina;
+create table empresa (
+	id int primary key auto_increment,
+    nomeFantasia varchar (45),
+    telefone char (11),
+    cnpj char (14),
+    razaoSocial varchar (45),
+    email varchar (45),
+    codigoAtivacao char (5)
+);
+
+create table cargo (
+	id int primary key auto_increment,
+    titulo varchar (45)
+);
+
+create table funcionario (
+	id int primary key auto_increment,
+    nome varchar (45),
+    email varchar (45),
+    senha varchar (45),
+    dataNascimento date,
+    fkEmpresa int, 
+    fkCargo int, 
+    constraint fkEmpresaFuncionario foreign key (fkEmpresa) references empresa (id),
+    constraint fkCargoFuncionario foreign key (fkCargo) references cargo (id)
+);
+
+SELECT * FROM maquina;
 SELECT nome FROM maquina WHERE id = 1;
 
-select * from registros;
--- select * from componentes;
+SELECT * FROM registros;
+
+
+
 
 
     
